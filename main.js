46c052c7d0d6d420165f04e82604a6f4c9df684d
MDCo <moonshotdesignco@gmail.com>
2021-01-20 14:14:07 -0500

    bg update

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-var closet = "https://closet.moonshotdesignco.com"
+// var closet = "https://closet.moonshotdesignco.com"
 
-var img = [
-  "djp/img/sm/portrait-cory04.jpg",
-  "djp/img/sm/portrait-cory05.jpg",
-  "djp/img/sm/portrait-cory06-2.jpg",
-  "djp/img/sm/portrait-cory09-2.jpg",
-  "djp/img/sm/portrait-cory10.jpg",
-  "djp/img/sm/portrait-cory12.jpg",
-  "djp/img/sm/portrait-cory14.jpg"
-]
+// var img = [
+//   "djp/img/sm/portrait-cory04.jpg",
+//   "djp/img/sm/portrait-cory05.jpg",
+//   "djp/img/sm/portrait-cory06-2.jpg",
+//   "djp/img/sm/portrait-cory09-2.jpg",
+//   "djp/img/sm/portrait-cory10.jpg",
+//   "djp/img/sm/portrait-cory12.jpg",
+//   "djp/img/sm/portrait-cory14.jpg"
+// ]
 
-var randBack = img[Math.floor(Math.random() * 7)];
+// var randBack = img[Math.floor(Math.random() * 7)];
 
-document.getElementById("bgdiv").style.backgroundImage = "url('" + closet + "/" + randBack + "')";
+// document.getElementById("bgdiv").style.backgroundImage = "url('" + closet + "/" + randBack + "')";
