bfe9140dc19ac3ba692561f49177a44253feb162
MDCo <moonshotdesignco@gmail.com>
2022-01-31 15:28:57 -0600

    modal + method + mgmt

--- main.js
+++ main.js
@@ -13,3 +13,22 @@
 // var randBack = img[Math.floor(Math.random() * 7)];
 
 // document.getElementById("bgdiv").style.backgroundImage = "url('" + closet + "/" + randBack + "')";
+
+
+// Get the modal
+var modal = document.getElementById("preSaveModal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
